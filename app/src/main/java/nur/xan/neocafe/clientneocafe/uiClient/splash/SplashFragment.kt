9149ff8f5e48075c4f8ce912f27542ce2772c794
NurXan <MXan000@gmail.com>
2023-11-08 20:02:36 +0600

    part 1

---
+++ app/src/main/java/nur/xan/neocafe/clientneocafe/uiClient/splash/SplashFragment.kt
@@ -0,0 +1,45 @@
+package nur.xan.neocafe.clientneocafe.uiClient.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import nur.xan.neocafe.clientneocafe.R
+import nur.xan.neocafe.clientneocafe.databinding.FragmentSplashBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_SplashFragment_to_AuthFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
